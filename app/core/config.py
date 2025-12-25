5af20a69082b65fdd40331e041d9935d5472c51b
gren-d-VVGolopuzov <gren-d-vvgolopuzov@gk.rosatom.local>
2025-12-26 00:51:28 +0300

    base-arch.v2 + alembic

--- app/core/config.py
+++ app/core/config.py
@@ -14,7 +14,22 @@ class Settings(BaseSettings):
     APP_NAME: str = "task-service"
     APP_ENV: str = "local"
 
-    DATABASE_URL: str
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+
+    DATABASE_URL: Optional[str] = None
     RABBITMQ_URL: Optional[str] = None
+
+    @property
+    def db_url(self) -> str:
+        if self.DATABASE_URL:
+            return self.DATABASE_URL
+        return (
+            f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 settings = Settings()
